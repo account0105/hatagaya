9841e6bf97320e36a96054be6c46ad83792e575c
shiobe <shiobeiji@gmail.com>
2024-04-04 15:25:57 +0900

    aa

--- app/libs/menu_cms.js
+++ app/libs/menu_cms.js
@@ -1,13 +1,13 @@
 import { createClient } from "microcms-js-sdk";
 
 // 環境変数のチェック
-if (!process.env.MICROCMS_SERVICE_DOMAIN) {
-    throw new Error("MICROCMS_SERVICE_DOMAIN is required");
-}
+// if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+//     throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+// }
 
-if (!process.env.MICROCMS_API_KEY) {
-    throw new Error("MICROCMS_API_KEY is required");
-}
+// if (!process.env.MICROCMS_API_KEY) {
+//     throw new Error("MICROCMS_API_KEY is required");
+// }
 
 // API取得用のクライアントを作成
 const client = createClient({
